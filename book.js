59db79bcef765a477fc4ca51d5cbc7a8f3233161
freefmly <52983002+freefmly@users.noreply.github.com>
2020-02-28 22:06:05 +0900

    Update

--- book.js
+++ book.js
@@ -6,7 +6,6 @@ const form = document.querySelector(".mustReadForm"),
 
 const BLMR_LS = "booksMustRead";
 let BLMR_Obj = [];
-let newId = "";
 
 function removeBookData(event) {
   const target = event.target;
@@ -25,56 +24,48 @@ function saveBookData() {
 
 function submitHandler(event) {
   event.preventDefault();
-  const bookTitleVal = bookTitle.value;
-  const bookAuthorVal = bookAuthor.value;
-  const bookPublisherVal = bookPublisher.value;
-  newId = BLMR_Obj.length + 1;
-  const bookInfo_Obj = {
-    id: newId,
-    Title: bookTitleVal,
-    Author: bookAuthorVal,
-    Publisher: bookPublisherVal
+  const currentValue = {
+    id: "",
+    Title: bookTitle.value,
+    Author: bookAuthor.value,
+    Publisher: bookPublisher.value
   };
-  BLMR_Obj.push(bookInfo_Obj);
-  saveBookData();
-  location.reload();
+  showBookList(currentValue);
   // 제출 버튼 누르면 입력값 리셋
   bookTitle.value = "";
   bookAuthor.value = "";
   bookPublisher.value = "";
 }
 
-function showBookList() {
-  const bookinfo_LS = localStorage.getItem(BLMR_LS);
-  const parsedBookData = JSON.parse(bookinfo_LS);
-  BLMR_Obj = parsedBookData;
-  /*
-  BLMR_Obj.forEach(function(item, index, array) {
-    console.log(item.Title);
-    console.log(item.Author);
-    console.log(item.Publisher);
-  });
-  */
-  for (let i = 0; i < BLMR_Obj.length; i++) {
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    const delBtn = document.createElement("i");
-    newId = i + 1;
-    li.id = newId;
-    span.innerText = `${BLMR_Obj[i].Title} (저자: ${BLMR_Obj[i].Author} / 출판사: ${BLMR_Obj[i].Publisher})`;
-    delBtn.classList.add("delBtn", "fas", "fa-ban");
-    delBtn.addEventListener("click", removeBookData);
-    delBtn.setAttribute("id", i + 1);
-    li.appendChild(span);
-    li.appendChild(delBtn);
-    bookListContainer.appendChild(li);
-  }
+function showBookList(Books) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const delBtn = document.createElement("i");
+  const newId = BLMR_Obj.length + 1;
+  span.innerText = `${Books.Title} (저자: ${Books.Author} / 출판사: ${Books.Publisher})`;
+  delBtn.classList.add("delBtn", "fas", "fa-ban");
+  delBtn.addEventListener("click", removeBookData);
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  li.id = newId;
+  bookListContainer.appendChild(li);
+  const bookInfo_Obj = {
+    id: newId,
+    Title: Books.Title,
+    Author: Books.Author,
+    Publisher: Books.Publisher
+  };
+  BLMR_Obj.push(bookInfo_Obj);
+  saveBookData();
 }
 
 function loadBookList() {
   const loadedBookList = localStorage.getItem(BLMR_LS);
   if (loadedBookList !== null) {
-    showBookList();
+    const parsedBookData = JSON.parse(loadedBookList);
+    parsedBookData.forEach(function(Books) {
+      showBookList(Books);
+    });
   }
 }
 
